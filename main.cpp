c4812081d6d1419a509222e6f0ba4afbae85163d
cianmcateer <cianmcateer@gmail.com>
2017-11-24 13:34:43 +0000

    First commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// C++ CA2 Cian McAteer Ciaran Maher
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+void init();
+
+int main(void) {
+    init();
+}
+
+void init() {
+    cout << "Hello" << endl;
+}
