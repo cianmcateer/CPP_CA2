188b5222e96f4ead0e4f9de27a4acce4ff4bc773
Ciaran Maher <ciaranmaher13@outlook.com>
2017-11-29 22:20:48 +0000

    office class

---
+++ Office.cpp
@@ -0,0 +1,17 @@
+#include "Office.h"
+
+Office::Office() {}
+
+Office::Office(std::string name, int age, int hours, std::string email, float salary)
+: Employee(name, age, hours), email(email), salary(salary) {}
+
+Office::~Office() {}
+
+void Office::print() {
+    std::cout << "Name: " << name << " Age: " << age << " Hours: " << hours << " Email: " << email << " Salary: " << salary << std::endl;
+}
+
+std::string Office::save() {
+    std::string details = name +", "+ std::to_string(age) +", "+ std::to_string(hours) +", "+email +", "+ std::to_string(salary);
+	return details;
+}
