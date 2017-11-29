088f7cc203bea6f0ddaccb7d1d4b9fffd6745c48
Ciaran Maher <ciaranmaher13@outlook.com>
2017-11-29 22:20:43 +0000

    office class

---
+++ Office.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+#include "Employee.h"
+
+class Office: public Employee {
+
+private:
+    std::string email;
+    float salary;
+
+public:
+
+    void print();
+	std::string save();
+    Office();
+    Office(std::string name, int age, int hours, std::string email, float salary);
+    virtual ~Office();
+
+    inline std::string getEmail() const {
+        return email;
+    }
+
+    inline float getSalary() const {
+        return salary;
+    }
+
+    inline void setEmail(std::string email) {
+        this->email = email;
+    }
+
+    inline void setSalary(float salary) {
+        this->salary = salary;
+    }
+
+};
