c7095e62a06949e8a12b1d9a9fa4ff54b2d72f26
Ciaran Maher <ciaranmaher13@outlook.com>
2017-11-29 22:17:39 +0000

    employee class

---
+++ Employee_Store.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <typeinfo>
+#include <algorithm>
+#include <fstream>
+
+#include "Employee.h"
+#include "Office.h"
+#include "Factory.h"
+
+class Employee_Store {
+
+private:
+    std::vector<Employee*> employeeStore;
+    std::vector<Employee*> load();
+public:
+    Employee_Store();
+    ~Employee_Store();
+
+
+    void print();
+	
+
+    std::vector<Employee*> getEmployeeStore() const;
+
+    bool in_range(int& i);
+    void add(Employee* e);
+    void show_index();
+	void erase(int& index);
+	void updateEmployee();
+	void sortEmployees();
+	std::string save();
+	void saveChanges();
+};
